0562181aeea8fb544bf7bcf63c3c7b6c292aa275
pddabcptitu <phamdinh11012004@gmail.com>
2026-03-08 23:29:58 +0700

    fix timeout crawler.py

--- scraper/crawler.py
+++ scraper/crawler.py
@@ -48,11 +48,18 @@ async def search_jobs(page, keyword, start=0):
 
 async def get_links(page):
     print("Lấy links...")
+    
     container = await page.query_selector("#main > div > div.scaffold-layout__list-detail-inner.scaffold-layout__list-detail-inner--grow > div.scaffold-layout__list > div")
     for _ in range(10):
         await container.evaluate("el => el.scrollTop += 1000")
         await page.wait_for_timeout(1000)
-    await page.wait_for_selector('div[data-job-id]')
+
+    try:
+        await page.wait_for_selector('div[data-job-id]', timeout=5000)
+    except:
+        print("Không còn job ở trang này")
+        await page.wait_for_timeout(random.choice([5000]))
+        return []
 
     elements = await page.query_selector_all('div[data-job-id]')
 
@@ -71,21 +78,24 @@ async def get_links(page):
     return list(set(links))
 
 async def get_job(page, link):
-    print("Đang lấy thông tin job từ link:", link)
-    await page.goto(link)
-    container = page.locator('#workspace > div > div > div._181b0d94.f53383eb.e1d14876 > div > div > div > div._0d26244a.b902da86._4ce9449f._4d8e8f8c._5a0aee88.ea12a7a2.a2ac8e20 > div > div > div.e8b95380 > div > div._0d26244a.b902da86._4ce9449f._4d8e8f8c._5a0aee88.ea12a7a2._60353d7a > div.b902da86._744bf2ab._5ba7d009.c2660157.ea12a7a2.a2ac8e20').first
-    company_name = await container.locator("div").locator("a").inner_text()
-    job_title_container = container.locator("+ div")
-    job_title = await job_title_container.inner_text()
-    location_container = job_title_container.locator("+ div + p")
-    # Gia Lâm, Hanoi, Vietnam · 1 tuần trước · 21 ứng viên
-    location= await location_container.inner_text()
-    location = location.split("·")[0].strip()
+    try:
+        print("Đang lấy thông tin job từ link:", link)
+        await page.goto(link)
+        container = page.locator('#workspace > div > div > div._181b0d94.f53383eb.e1d14876 > div > div > div > div._0d26244a.b902da86._4ce9449f._4d8e8f8c._5a0aee88.ea12a7a2.a2ac8e20 > div > div > div.e8b95380 > div > div._0d26244a.b902da86._4ce9449f._4d8e8f8c._5a0aee88.ea12a7a2._60353d7a > div.b902da86._744bf2ab._5ba7d009.c2660157.ea12a7a2.a2ac8e20').first
+        company_name = await container.locator("div").locator("a").inner_text()
+        job_title_container = container.locator("+ div")
+        job_title = await job_title_container.inner_text()
+        location_container = job_title_container.locator("+ div + p")
+        # Gia Lâm, Hanoi, Vietnam · 1 tuần trước · 21 ứng viên
+        location= await location_container.inner_text()
+        location = location.split("·")[0].strip()
 
-    job_container = page.locator('#workspace > div > div > div._181b0d94.f53383eb.e1d14876 > div > div > div > div._0d26244a.b902da86._4ce9449f._4d8e8f8c._5a0aee88.ea12a7a2.c69e5dea > div:nth-child(3) > div > div > div > div > div > p > span')
+        job_container = page.locator('#workspace > div > div > div._181b0d94.f53383eb.e1d14876 > div > div > div > div._0d26244a.b902da86._4ce9449f._4d8e8f8c._5a0aee88.ea12a7a2.c69e5dea > div:nth-child(3) > div > div > div > div > div > p > span')
 
-    job_description = await job_container.inner_text()
-
+        job_description = await job_container.inner_text()
+    except Exception as e:
+        print(f"Lỗi khi lấy thông tin job từ link {link}: {e}")
+        return None
     return {
         "link": link,
         "title": job_title,
@@ -108,11 +118,15 @@ async def get_all_links(page):
 
     links = set()
     for start in range(0, total_jobs, 25):
-        await search_jobs(page, quote('Công nghệ thông tin'), start)
-        new_links = await get_links(page)
-        print(f"Đã lấy {len(new_links)} links từ trang {start//25 + 1}")
-        links.update(new_links)
-        await page.wait_for_timeout(random.choice([1000, 1500, 2000]))
+        try:
+            await search_jobs(page, quote('Công nghệ thông tin'), start)
+            new_links = await get_links(page)
+            print(f"Đã lấy {len(new_links)} links từ trang {start//25 + 1}")
+            links.update(new_links)
+            await page.wait_for_timeout(random.choice([1000, 1500, 2000]))
+        except Exception as e:
+            print(f"Lỗi khi lấy links từ trang {start//25 + 1}: {e}")
+            await page.wait_for_timeout(5000)
 
     return list(links)
 
@@ -135,6 +149,8 @@ async def scraper_linkedin():
         links = await get_all_links(page)
         for link in links:
             job = await get_job(page, link)
+            if not job:
+                continue
             await asyncio.sleep(random.uniform(1, 3))
             jobs.append(job)
         save_jobs_to_csv(jobs)
